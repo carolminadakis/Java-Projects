f5f0f735fb640d20b6a380ccff3e4f7f41b0af5c
Ana Carolina Minadakis <carolminadakis@gmail.com>
2020-06-30 17:26:13 -0300

    Projeto teste para inclusão no Github via Eclipse

---
+++ HoraDoSistema/src/horadosistema/HoraDoSitema.java
@@ -0,0 +1,15 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSitema {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date relogio = new Date();
+		System.out.print("A hora do sistema é: ");
+		System.out.println(relogio.toString());
+
+	}
+
+}
